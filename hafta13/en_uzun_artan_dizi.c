57b352f3040efb949b06a84f0cf4494d003772c7
metehanytu <metehan.turan@yildiz.edu.tr>
2024-12-25 14:01:12 +0300

    Create en_uzun_artan_dizi.c

---
+++ hafta13/en_uzun_artan_dizi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void find_longest_increasing_subarray(int arr[], int size);
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 5, 4, 7, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    find_longest_increasing_subarray(arr, size);
+
+    return 0;
+}
+
+void find_longest_increasing_subarray(int arr[], int size) {
+    // Kodunuzu buraya yazın
+}

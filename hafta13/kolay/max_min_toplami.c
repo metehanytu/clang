db82464361056b212e2c8fe6cd831f03cd7b71fa
metehanytu <metehan.turan@yildiz.edu.tr>
2024-12-25 14:03:00 +0300

    Create max_min_toplami.c

---
+++ hafta13/kolay/max_min_toplami.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 5, 8, 99, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // En küçük ve en büyük elemanları bulan kod buraya gelecek
+
+    return 0;
+}

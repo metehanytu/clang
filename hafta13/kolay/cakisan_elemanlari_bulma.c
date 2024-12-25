f0aa67765092ca477141dab1f904936c47a32318
metehanytu <metehan.turan@yildiz.edu.tr>
2024-12-25 14:04:11 +0300

    Create cakisan_elemanlari_bulma.c

---
+++ hafta13/kolay/cakisan_elemanlari_bulma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[] = {1, 3, 5, 7, 9};
+    int arr2[] = {2, 4, 6, 8, 9};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    // Çakışan elemanları bulan kod buraya gelecek
+
+    return 0;
+}

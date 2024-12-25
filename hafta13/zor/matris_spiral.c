c5c9ee0120d86eb5d398b63b867325904a0ed7d9
metehanytu <metehan.turan@yildiz.edu.tr>
2024-12-25 14:07:52 +0300

    Create matris_spiral.c

---
+++ hafta13/zor/matris_spiral.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_spiral(int matrix[4][4]);
+
+int main() {
+    int matrix[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+    };
+
+    print_spiral(matrix);
+
+    return 0;
+}
+
+void print_spiral(int matrix[3][3]) {
+    // Kodunuzu buraya yazın
+    // Çıktı 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 şeklinde olmalı
+    // Elemanları teker teker yazdıran kod kabul edilmeyecektir.
+}
